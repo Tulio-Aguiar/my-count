4adb5d848bd62cc31b96facd527c415bc9d6ac75
Túlio Aguiar <67409937+Tulio-Aguiar@users.noreply.github.com>
2021-11-08 13:50:38 +0100

    my counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
+
 function App() {
+
+  const [ contagem, setContagem ] = useState( 0 );
+
+  const botaoAction = () => {
+    setContagem(contagem +1);
+  };
+
+  const botaoMenos = () =>{
+    setContagem(contagem -1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1> My counter </h1>
+      <div className="content">
+      <div className="count">{contagem} vezes</div>
+      <button className="btn" onClick={botaoAction}> Clique para aumentar</button>
+      <hr />
+      <button className="btn" onClick={botaoMenos}>  Clique para subtrair</button>
+    </div>
     </div>
   );
 }
